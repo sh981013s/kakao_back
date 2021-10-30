a011ce22c7c5ed47e1088de0be11be42cf4d1692
beom <hblee@safetics.net>
2021-10-31 01:11:46 +0900

    member sql

--- server/module/friend.js
+++ server/module/friend.js
@@ -5,7 +5,7 @@ const router = express.Router();
 const randtoken = require('rand-token');
 
 
-router.post("/signup", (req, res) => {
+router.get("/getFriends", (req, res) => {
     // select a.uid, a.name, a.birth, a.pic, a.state  from member a inner join friends b on a.uid = b.friend where b.owner = 7
     db.query('select a.uid, a.name, a.birth, a.pic, a.state  from member a inner join friends b on a.uid = b.friend where b.owner = ?', [7], (err, rows) => {
         res.send(rows);
