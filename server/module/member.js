478241cd5b0d22293c9359cd97a772547bdbbce7
beom <hblee@safetics.net>
2021-10-31 00:28:46 +0900

    member sql

--- server/module/member.js
+++ server/module/member.js
@@ -22,43 +22,14 @@ router.get('/getFriends', (req, res) => {
 router.post('/signin', (req, res) => {
     const {email, pw} = req.body;
     db.query('SELECT * FROM member where email = ? and pw = ? ', [email, pw], (err, rows) => {
-    	if (err) {
-    		throw err;
-    	}
+        if (err) {
+            throw err;
+        }
         if(rows.length === 1){
             const token = randtoken.uid(256);
             db.query('UPDATE member SET token = ? WHERE uid = ?', [token, rows[0].uid]);
             let parameter = {
                 accessToken : token,
-                 resultType : 'success'
-            }
-            res.send(parameter);
-        }else{
-            let parameter = {
-                resultType : 'fail'
-            }
-            res.send(parameter);
-        }
-    });
-});
-
-router.post("/me", (req, res) => {
-    let value = req.body;
-    console.log(req,':::')
-
-});
-
-
-router.post("/signup", (req, res) => {
-    let value = req.body;
-    // bcryptjs.genSalt(10, (err, salt) => {
-    db.query('SELECT * FROM member where token = ?', [value], (err, rows) => {
-        if (err) {
-            throw err;
-        }
-        if(rows.length === 1){
-            let parameter = {
-                userInfo : rows,
                 resultType : 'success'
             }
             res.send(parameter);
@@ -68,9 +39,36 @@ router.post("/signup", (req, res) => {
             }
             res.send(parameter);
         }
-
-
     });
+});
+
+router.get("/me", (req, res) => {
+    console.log(req,'::::')
+    // db.query('SELECT * FROM member where token = ?', [value], (err, rows) => {
+    //     if (err) {
+    //         throw err;
+    //     }
+    //     if(rows.length === 1){
+    //         let parameter = {
+    //             userInfo : rows,
+    //             resultType : 'success'
+    //         }
+    //         res.send(parameter);
+    //     }else{
+    //         let parameter = {
+    //             resultType : 'fail'
+    //         }
+    //         res.send(parameter);
+    //     }
+    // });
+
+});
+
+
+router.post("/signup", (req, res) => {
+    let value = req.body;
+    // bcryptjs.genSalt(10, (err, salt) => {
+
 });
 
 
