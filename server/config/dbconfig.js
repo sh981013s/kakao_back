6215acbf913fc2cd0c7cc4ed931d52826f63f113
SeungHwan-Lee <sh981013s@gmail.com>
2021-10-30 22:26:51 +0900

    k

--- server/config/dbconfig.js
+++ server/config/dbconfig.js
@@ -1,7 +1,7 @@
 const dbconfig = {
-	host: 'localhost',
-	user: 'root',
-	password: '12345678',
+	host: 'kakaoproject3952.mysql.database.azure.com',
+	user: 'kakao@kakaoproject3952',
+	password: 'vlzkcb1324!',
 	port: 3306,
 	database: 'kakao_friends',
 };
